30a851233c8325e0cad1149fe753f8928eb7ee3c
Blader-1 <cristianpino699@gmail.com>
2022-09-20 13:18:58 -0500

    Subido

--- app.js
+++ app.js
@@ -2,6 +2,28 @@ const ROCK = "rock";
 const PAPER = "paper";
 const SCISSORS = "scissors";
 
+const PIE = "rock";
+const PAP = "paper";
+const TIJ = "scissors";
+
+
+var punto_jugador1;
+var punto_jugador2;
+var txtj1
+var txtj2
+
+var jugador1 = document.getElementById("jugador-01");
+var jugador2 = document.getElementById("jugador-02");
+jugador1.addEventListener("keyup", (event) => {
+    txtj1 = event.target.value;
+});
+jugador2.addEventListener("keyup", (event) => {
+    txtj2 = event.target.value;
+});
+
+const m_puntos1 = document.getElementById("puntos1");
+const m_puntos2 = document.getElementById("puntos2");
+
 const TIE = 0;
 const WIN = 1;
 const LOST = 2;
@@ -11,51 +33,101 @@ let isPlaying = false;
 
 const playBtn = document.getElementById("boton-play");
 
-function registro(){
-    var jugador1 = document.forms["registro-jugadores"]["jugador1"].value;
-    var jugador2 = document.forms["registro-jugadores"]["jugador2"].value;
-    var validacion = true;
 
-    if (jugador1 == "") {
-        console.log("El campo jugador 1 esta vacio");
-        validacion = false;
-    }
-    if (jugador2 == "") {
-        console.log("El campo jugador 1 esta vacio");
-        validacion = false;
-    }
-    if (!validacion) {
-        return false;
-    }
-}
+    playBtn.addEventListener("click", () => {
+
+        if (txtj1 == undefined) {
+            // alert(`Campo player1 vacio`);
+        } else if(jugador1 =! undefined && txtj2 == undefined){
+             alert(`El Juego ha iniciado para ${txtj1}, Diviertete!!`)
+            jugador_01();
+        } 
+        if(txtj2 == undefined) {
+            // alert(`campo del Player2 vacia`);
+        }else if(jugador2 =! undefined && txtj1 == undefined){
+            alert(`El Juego ha iniciado para ${txtj2}, Diviertete!!`);
+            jugador_02();
+        }
+        if (txtj1 != undefined && txtj2 != undefined){
+            alert(`El Juego ah iniciado Para ${txtj1} y ${txtj2}, Feliz Juego!!`);
+            jugador_01();
+            jugador_02();
+        }
+    });
+
+
+
+
+
 
 const rockBtn = document.getElementById("piedra");
 const paperBtn = document.getElementById("papel");
 const scissorsBtn = document.getElementById("tijeras");
+
+const pieBtn = document.getElementById("pie");
+const papBtn = document.getElementById("pap");
+const tijBtn = document.getElementById("tij");
+
 const resultText = document.getElementById("text-inicio");
-const userImg = document.getElementById("usurio-img");
+const userImg = document.getElementById("jugador01-img");
+const user2Img = document.getElementById("jugador02-img");
 const machineImg = document.getElementById("maquina-img");
+const machine2Img = document.getElementById("maquina2-img");
 
-rockBtn.addEventListener("click", () => {
-    play(ROCK);
-});
-paperBtn.addEventListener("click", () => {
-    play(PAPER);
-});
-scissorsBtn.addEventListener("click", () => {
-    play(SCISSORS);
-});
 
-function play(userOption) {
-    if(isPlaying) return;
+function jugador_01() {
+
+
+    rockBtn.addEventListener("click", () => {
+        play_01(ROCK);
+    });
+    paperBtn.addEventListener("click", () => {
+        play_01(PAPER);
+    });
+    scissorsBtn.addEventListener("click", () => {
+        play_01(SCISSORS);
+    });
+    
+}
+// if (jugador1 == null) {
+//     //alert("Ingrese el nombre del jugador 1")
+// } else if (jugador1 != null) {
+
+// }
+
+function jugador_02() {
+
+
+    pieBtn.addEventListener("click", () => {
+        play_02(PIE);
+    });
+    papBtn.addEventListener("click", () => {
+        play_02(PAP);
+    });
+    tijBtn.addEventListener("click", () => {
+        play_02(TIJ);
+    });
+}
+
+// function turnos(){
+
+//     if(txtj1 != undefined){
+//         play_01();
+
+//     }
+// }
+
+function play_01(userOption) {
+    if (isPlaying) return;
 
     isPlaying = true;
+    
 
     userImg.src = "img/" + userOption + ".svg";
 
     resultText.innerHTML = "Escogiendo!";
 
-    const interval = setInterval(function(){
+    const interval = setInterval(function () {
         const machineOption = calcMachineOption();
         machineImg.src = "img/" + machineOption + ".svg";
     }, 200);
@@ -74,6 +146,8 @@ function play(userOption) {
                 resultText.innerHTML = "Has Empatado!";
                 break;
             case WIN:
+                // punto_jugador1 += 1;
+                // m_puntos1.innerHTML = `${jugador1} puntos: ${punto_jugador1}`;
                 resultText.innerHTML = "Ganaste!";
                 break;
             case LOST:
@@ -84,7 +158,61 @@ function play(userOption) {
     }, 2000);
 }
 
+function play_02(user2Option) {
+    if (isPlaying) return;
+
+    isPlaying = true;
+
+    user2Img.src = "img2/" + user2Option + ".svg";
+
+    resultText.innerHTML = "Escogiendo!";
+
+    const interval = setInterval(function () {
+        const machine2Option = calcMachine2Option();
+        machine2Img.src = "img2/" + machine2Option + ".svg";
+    }, 200);
+
+    setTimeout(function () {
+
+        clearInterval(interval);
+
+        const machine2Option = calcMachine2Option();
+        const result = calcResult(user2Option, machine2Option);
+
+        machine2Img.src = "img2/" + machine2Option + ".svg";
+
+        switch (result) {
+            case TIE:
+                resultText.innerHTML = "Has Empatado!";
+                break;
+            case WIN:
+                // punto_jugador1 += 1;
+                // m_puntos1.innerHTML = `${jugador1} puntos: ${punto_jugador1}`;
+                resultText.innerHTML = "Ganaste!";
+                break;
+            case LOST:
+                resultText.innerHTML = "Perdiste!";
+                break;
+        }
+        isPlaying = false;
+    }, 2000);
+}
+
+
+
 function calcMachineOption() {
+    const number = Math.floor(Math.random() * 3);
+    switch (number) {
+        case 0:
+            return ROCK;
+        case 1:
+            return PAPER;
+        case 2:
+            return SCISSORS;
+    }
+}
+
+function calcMachine2Option() {
     const number = Math.floor(Math.random() * 3);
     switch (number) {
         case 0:
@@ -117,3 +245,4 @@ function calcResult(userOption, machineOption) {
 
     }
 }
+
